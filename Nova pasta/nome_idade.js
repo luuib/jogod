50efc57cc91b09938e9470f7628d7179d9195f45
luuib <129198298+luuib@users.noreply.github.com>
2024-02-23 11:57:46 -0300

    Add files via upload

---
+++ Nova pasta/nome_idade.js
@@ -0,0 +1,8 @@
+let nome = 'Luiz';
+let idade = 17;
+let peso = '70';
+let altura = '1,80';
+let numero  = 8; 
+
+console.log(`O meu nome é ${nome} e tenho ${idade} `);
+console.log(`A idade da pessoa mais o numero é ${idade + numero} , peso ${peso} e tenho ${altura}`)
---
+++ Nova pasta/number.js
@@ -0,0 +1,9 @@
+let inteiro = 10;
+let flutuante = 2.5;
+let inteiro2 = 30;
+let flutuante2 = 30.5;
+
+console.log(inteiro);
+console.log(inteiro2);
+console.log(flutuante);
+console.log(flutuante2);
